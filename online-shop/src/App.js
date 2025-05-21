21126fc22d7cdff3ec41079f9f9fcd68ce28952a
Kira <kirnan_t@outlok.com>
2025-05-21 22:27:03 +0700

    изменение app

--- online-shop/src/App.js
+++ online-shop/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ShopProvider } from './context/ShopContext';
+import ProductList from './pages/ProductList';
+import Cart from './pages/Cart';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        ShopProvider > { /* остальной код */ } <
+        /ShopProvider>
+    );
 }
 
 export default App;
